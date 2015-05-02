408dbd023d80456420c79cc491f5c10a14b328a3
Blundell <blundell.app.dev@gmail.com>
2015-05-02 15:23:49 +0100

    remove unused field

--- demo/src/main/java/com/blundell/quicksand/demo/MainActivity.java
+++ demo/src/main/java/com/blundell/quicksand/demo/MainActivity.java
@@ -14,8 +14,6 @@ import com.blundell.quicksand.demo.viewanimation.ViewAnimateActivity;
 
 public class MainActivity extends Activity {
 
-    private CharSequence title;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
